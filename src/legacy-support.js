7ee4b9191f4a57f368200bb4bb4fc71ab45d650a
ivan baktsheev <dot.and.thing@gmail.com>
2019-05-09 00:01:20 +0300

    node < 6 now considered legacy and requires some legwork.

--- src/legacy-support.js
+++ src/legacy-support.js
@@ -1,5 +1,31 @@
 var nodeVer = process.version.substr (1).split ('.');
 
+if (nodeVer[0] >= 6)
+	return;
+
+var legacyModulesInstallCmd = 'npm install object-assign buffer-indexof-polyfill';
+
+if (require.main === module) {
+	var spawn = require ('child_process').spawn;
+
+	var child = spawn (legacyModulesInstallCmd);
+	child.stdout.pipe (process.stdout);
+	child.stderr.pipe (process.stderr);
+
+	child.on ('error', function (err) {
+		process.exit (1);
+	});
+
+	child.on ('exit', function (code) {
+		process.exit (0);
+	});
+
+    return;
+}
+
+
+try {
+
 if (nodeVer[0] < 4) {
 	global.Promise = global.Promise || require ('bluebird');
 	Object.assign  = Object.assign  || require ('object-assign');
@@ -10,4 +36,11 @@ if (nodeVer[0] < 4) {
 
 if (nodeVer[0] < 6) {
 	require ('buffer-indexof-polyfill');
+}
+
+} catch (err) {
+	console.warn ("You're using outdated nodejs version.");
+	console.warn ("This module supports nodejs down to the version 0.10, but some legwork required.");
+	console.warn ("Either install version >= 6, or add dependencies to your own project with `" + legacyModulesInstallCmd + "`");
+
 }
