0df503bf795fb8db1f3d6558140e7a8f7f449c87
nezed [CustoMac] <nezed@ya.ru>
2019-05-26 23:23:03 +0300

    Fix main entry

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 try {
   var nodeVer = process.version.substr(1).split ('.')
   if (nodeVer[0] >= 6) {
-    return module.exports = require('lib/clickhouse')
+    return module.exports = require('./lib/clickhouse')
   }
 } catch (e) {}
 
 // Tries to import library built for legacy nodejs
 try {
-  module.exports = require('lib-legacy/clickhouse')
+  module.exports = require('./lib-legacy/clickhouse')
 } catch (e) {
   // If all imports are failed it may be dev enviroment
-  module.exports = require('src/clickhouse')
+  module.exports = require('./src/clickhouse')
 }
