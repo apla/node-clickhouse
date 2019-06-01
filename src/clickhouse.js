7ffac1b853fbd4ee5fa1649d4d7656037c6012ce
nezed [CustoMac] <nezed@ya.ru>
2019-06-01 23:38:01 +0300

    Keep useQueryString backwards compatibility

--- src/clickhouse.js
+++ src/clickhouse.js
@@ -248,7 +248,7 @@ ClickHouse.prototype.query = function (chQuery, options, cb) {
 	options.omitFormat  = options.omitFormat  || this.options.omitFormat  || false;
 	options.dataObjects = options.dataObjects || this.options.dataObjects || false;
 	options.format      = options.format      || this.options.format      || null;
-	options.readonly    = options.readonly    || this.options.readonly    || false;
+	options.readonly    = options.readonly    || this.options.readonly    || this.options.useQueryString || false;
 
 	// we're adding `queryOptions` passed for constructor if any
 	var queryObject = Object.assign ({}, this.options.queryOptions, options.queryOptions);
